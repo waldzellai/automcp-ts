44e5f6f21b13adc873d79d528a623ff9d3c3739b
moarshy <marshath@gmail.com>
2025-04-04 21:41:59 +0800

    Add SimpleResearcher example: Introduced a new crew implementation with agents and tasks for AI research, including configuration files for agents and tasks, a main execution script, and an environment sample file.

---
+++ examples/crewai/simple_researcher/crew.py
@@ -0,0 +1,48 @@
+# src/latest_ai_development/crew.py
+from crewai import Agent, Crew, Process, Task
+from crewai.project import CrewBase, agent, crew, task
+from crewai_tools import SerperDevTool
+
+@CrewBase
+class SimpleResearcherCrew():
+  """Simple Researcher crew"""
+
+  agents_config = "./config/agents.yml"
+  tasks_config = "./config/tasks.yml"
+
+  @agent
+  def researcher(self) -> Agent:
+    return Agent(
+      config=self.agents_config['researcher'],
+      verbose=True,
+      tools=[SerperDevTool()]
+    )
+
+  @agent
+  def reporting_analyst(self) -> Agent:
+    return Agent(
+      config=self.agents_config['reporting_analyst'],
+      verbose=True
+    )
+
+  @task
+  def research_task(self) -> Task:
+    return Task(
+      config=self.tasks_config['research_task'],
+    )
+
+  @task
+  def reporting_task(self) -> Task:
+    return Task(
+      config=self.tasks_config['reporting_task'],
+    )
+
+  @crew
+  def crew(self) -> Crew:
+    """Creates the LatestAiDevelopment crew"""
+    return Crew(
+      agents=self.agents, # Automatically created by the @agent decorator
+      tasks=self.tasks, # Automatically created by the @task decorator
+      process=Process.sequential,
+      verbose=True,
+    )
